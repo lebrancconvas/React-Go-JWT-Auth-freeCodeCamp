b01affdccf707a0755aa9212981be2843e1bc3fe
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 00:49:59 +0700

    🔨 Add Cookie.

--- controllers/authController.go
+++ controllers/authController.go
@@ -70,5 +70,16 @@ func Login(c *fiber.Ctx) error {
 		}) 
 	}
 
-	return c.JSON(token)  
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour * 24), 
+		HTTPOnly: true, 
+	}
+
+	c.Cookie(&cookie)  
+
+	return c.JSON(fiber.Map{
+		"message": "Login Success", 
+	})  
 }
