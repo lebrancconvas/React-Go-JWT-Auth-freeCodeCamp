2659b4174c5958c949dce43cf9a26aca05ce1379
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 00:00:25 +0700

    🔨Login Script Success.

--- controllers/authController.go
+++ controllers/authController.go
@@ -25,3 +25,31 @@ func Register(c *fiber.Ctx) error {
 
 	return c.JSON(user)  
 } 
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string  
+	
+	if err := c.BodyParser(&data); err != nil {
+		return err 
+	} 
+
+	var user models.User 
+
+	database.DB.Where("username = ?", data["username"]).First(&user)  
+
+	if user.ID == 0 {
+		c.Status(fiber.StatusNotFound) 
+		return c.JSON(fiber.Map{
+			"message": "User not found.",  
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect Password.",  
+		})
+	} 
+
+	return c.JSON(user) 
+}
