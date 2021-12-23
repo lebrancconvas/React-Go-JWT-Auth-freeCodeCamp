d3002cbd8fb4d8279863e38433c6df662f107e5d
lebrancconvas <lebrancconvas@gmail.com>
2021-12-23 23:16:52 +0700

    🔨 Connect with Data Model.

--- database/connection.go
+++ database/connection.go
@@ -1,13 +1,16 @@
 package database 
 import (
+	"github.com/lebrancconvas/React-Go-JWT-Auth-freeCodeCamp/models" 
 	"gorm.io/gorm"
 	"gorm.io/driver/mysql"  
 )
 
 func Connect() { 
-		_, err := gorm.Open(mysql.Open("root@/reactgo-auth"), &gorm.Config{})  
+		connection, err := gorm.Open(mysql.Open("root@/reactgo-auth"), &gorm.Config{})   
 
 		if err != nil {
 			panic("Couldn't connect with the database.") 
 		}
+
+		connection.AutoMigrate(&models.User{})  
 }
