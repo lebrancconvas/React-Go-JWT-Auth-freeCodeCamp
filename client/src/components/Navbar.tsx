4f13a8f0962de1b4cee73592d379c0d7c632936a
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 12:47:26 +0700

    🚚 Add Routes to Navbar and Homepage.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import React from 'react'; 
 import {Typography, AppBar, Toolbar, Button} from '@mui/material';  
 import {Box} from '@mui/system'; 
+import {useNavigate} from 'react-router-dom'; 
 
 const Navbar = () => {
+	const navigate = useNavigate(); 
+	const toRegister = () => navigate('/register'); 
+	const toLogin = () => navigate('/login');  
 	return (
 		<div>
 			<Box sx={{flexGrow: 1}}>
@@ -11,10 +15,10 @@ const Navbar = () => {
 						<Typography variant="h6" component="div" sx={{flexGrow: 1}}> 
 							React-Go-Auth 
 						</Typography>
-						<Button color="inherit"> 
+						<Button color="inherit" onClick={toRegister}>  
 							Register 
 						</Button>
-						<Button color="inherit">
+						<Button color="inherit" onClick={toLogin}> 
 							Login 
 						</Button>
 					</Toolbar>
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,10 +1,27 @@
 import React from 'react'; 
+import {Typography, Button} from '@mui/material'; 
+import {Box} from '@mui/system'; 
 import Navbar from '../components/Navbar'; 
+import {useNavigate} from 'react-router-dom'; 
 
 const Homepage = () => {
+	const navigate = useNavigate(); 
+	const toRegister = () => navigate('/register');  
+	const toLogin = () => navigate('/login'); 
 	return (
 		<div>
-			<Navbar /> 
+			<Box>
+				<Navbar /> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={2}> 
+				<Typography variant="h3"> 
+					Welcome to the website 
+				</Typography>
+			</Box>
+			<Box sx={{textAlign: 'center'}}>
+				<Button variant="contained" onClick={toRegister}>Register</Button> 
+				<Button variant="contained" onClick={toLogin}>Login</Button>  
+			</Box>
 		</div>
 	)
 }
