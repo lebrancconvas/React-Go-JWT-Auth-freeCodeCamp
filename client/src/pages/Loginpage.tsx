75b80295ab7059f1ab81987db9ef24fad0e7ffc9
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 12:28:56 +0700

    💄 Add Navbar on Login Page.

--- client/src/pages/Loginpage.tsx
+++ client/src/pages/Loginpage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'; 
 import {Typography, Button, TextField} from '@mui/material';
 import {Box} from '@mui/system'; 
+import Navbar from '../components/Navbar'; 
 
 const Loginpage = () => {
 	return (
 		<div>
-			<Box sx={{textAlign: 'center'}}>  
+			<Box>
+				<Navbar /> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={2}>   
 				<Typography variant="h2"> 
 					Login 
 				</Typography>
