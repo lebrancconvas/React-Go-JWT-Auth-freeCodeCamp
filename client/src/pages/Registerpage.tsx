076b2006b02c41401691119e0c9f42759d9c8dee
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 13:29:41 +0700

    ✨ Register Feature Success.

--- client/src/pages/Registerpage.tsx
+++ client/src/pages/Registerpage.tsx
@@ -1,9 +1,34 @@
-import React from 'react'; 
+import React, {SyntheticEvent, useState} from 'react'; 
+import {Navigate} from 'react-router-dom';  
 import {Typography, Button, TextField} from '@mui/material';
 import {Box} from '@mui/system'; 
 import Navbar from '../components/Navbar'; 
 
 const Registerpage = () => {
+	const [username, setUsername] = useState(''); 
+	const [password, setPassword] = useState(''); 
+	const [redirect, setRedirect] = useState(false); 
+	const registerSubmit = async(e: SyntheticEvent) => {
+		e.preventDefault(); 
+
+		await fetch('http://localhost:3002/api/register', {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				username,
+				password 
+			})
+		}); 
+		
+		setRedirect(true); 
+		//const content = response.json(); 
+		//console.log(content); 
+	}; 
+
+	if(redirect) {
+		return <Navigate to="/login" />  	
+	}
+
 	return (
 		<div>
 			<Box>
@@ -15,15 +40,15 @@ const Registerpage = () => {
 				</Typography>
 			</Box>
 			<Box mt={3} sx={{textAlign: 'center'}}>  
-				<form action="">
+				<form> 
 					<Box>
-						<TextField style={{width: '50%'}} type="text" placeholder="Username" required />  
+						<TextField style={{width: '50%'}} type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} required />  
 					</Box>
 					<Box mt={1}>
-						<TextField style={{width: "50%"}} type="password" placeholder="Password" required /> 
+						<TextField style={{width: "50%"}} type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} required /> 
 					</Box>
 					<Box mt={2}> 
-						<Button type="submit" variant="contained">Register</Button> 
+						<Button type="submit" variant="contained" onClick={registerSubmit}>Register</Button> 
 					</Box>
 				</form>
 			</Box>
