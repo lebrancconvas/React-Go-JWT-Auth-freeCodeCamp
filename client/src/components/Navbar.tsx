791a345dac38540b3dc520be0afa846452ae8679
lebrancconvas <lebrancconvas@gmail.com>
2021-12-24 12:28:33 +0700

    💄 Seperate Navbar Section.

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'; 
+import {Typography, AppBar, Toolbar, Button} from '@mui/material';  
+import {Box} from '@mui/system'; 
+
+const Navbar = () => {
+	return (
+		<div>
+			<Box sx={{flexGrow: 1}}>
+				<AppBar position="static"> 
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={{flexGrow: 1}}> 
+							React-Go-Auth 
+						</Typography>
+						<Button color="inherit"> 
+							Register 
+						</Button>
+						<Button color="inherit">
+							Login 
+						</Button>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</div>
+	)
+}
+
+export default Navbar; 
