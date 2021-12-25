06b538bb3bc765570578b0008f4e7bf7ad06c3b8
lebrancconvas <lebrancconvas@gmail.com>
2021-12-25 18:43:49 +0700

    💄 Create Popup Component.

---
+++ client/src/components/popup/IncorrectPassword.tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const IncorrectPassword = () => {
+	return (
+		<div></div> 
+	);  
+}
+
+export default IncorrectPassword; 
---
+++ client/src/components/popup/UserNotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const UserNotFound = () => {
+	return (
+		<div></div> 
+	);  
+}
+
+export default UserNotFound; 
